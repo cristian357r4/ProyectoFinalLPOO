c52bf0fa0415b11e1acb154959cbcb3f5013acba
cristian357r4 <cristian357r4@gmail.com>
2020-06-16 21:24:16 -0500

    patch

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #  primer parametro como lo encontramos en la aplicacion segundo parametro: (home_controller) home#login este
   # parametro se agrega como funcion en el controller
+  resources :personas, :infousuarios, :nutriologos
+
+  
   root 'home#login'
 
 end
